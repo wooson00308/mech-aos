fd4f08b5f06aff3b92605303c63044b1d4bb932d
이신 <u77547957@gmail.com>
2024-09-29 15:03:01 +0900

    방어코드

--- Assets/QuantumUser/View/UI/GameUI.cs
+++ Assets/QuantumUser/View/UI/GameUI.cs
@@ -405,6 +405,7 @@ public unsafe class GameUI : QuantumSceneViewComponent<CustomViewContext>
         {
             if (_localEntityRef == EntityRef.None || !f.Has<Status>(_localEntityRef)) return;
             SkillInventory* skillInventory = f.Unsafe.GetPointer<SkillInventory>(_localEntityRef);
+            if (skillInventory->Skills == null) return;
             var skills = f.ResolveList(skillInventory->Skills);
             for (int i = 0; i < skills.Count; i++)
             {
